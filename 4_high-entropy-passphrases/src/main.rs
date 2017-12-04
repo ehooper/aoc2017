4778cd34567eae926f4a77386e8fa12db61c246b
ehooper <ehooper@umich.edu>
2017-12-04 15:06:47 -0500

    Day 4 part two

--- 4_high-entropy-passphrases/src/main.rs
+++ 4_high-entropy-passphrases/src/main.rs
@@ -1,10 +1,28 @@
 use std::io;
 use std::collections::hash_set::HashSet;
 
-fn is_valid(passphrase : &str) -> bool {
-    let mut hs = HashSet::<String>::new();
+// Assuming no more that 255 instances of the same character in a word.
+#[derive(PartialEq, Eq, Hash)]
+struct AnagramSet {
+    count : [u8; 26],
+}
+
+impl<'a> From<&'a str> for AnagramSet {
+    fn from(s : &'a str) -> Self {
+        let mut aset = AnagramSet{count: [0; 26]};
+        for &c in s.as_bytes() {
+            if c >= b'a' && c <= b'z' {
+                aset.count[(c - b'a') as usize] += 1;
+            }
+        }
+        return aset;
+    }
+}
+
+fn is_valid<'a, T : Eq + std::hash::Hash + From<&'a str>>(passphrase : &'a str) -> bool {
+    let mut hs = HashSet::<T>::new();
     for word in passphrase.split_whitespace() {
-        if hs.insert(word.to_owned()) == false {
+        if hs.insert(T::from(word)) == false {
             return false;
         }
     }
@@ -12,6 +30,9 @@ fn is_valid(passphrase : &str) -> bool {
 }
 
 fn main() {
+    let arg = std::env::args().nth(1);
+    let no_anagrams = arg.map_or(false, |s| s == "--no-anagrams");
+
     let mut input = String::new();
     println!("Enter passphrase list:");
     let mut count : usize = 0;
@@ -20,7 +41,11 @@ fn main() {
             println!("{} valid", count);
             println!("Enter passphrase list:");
             count = 0;
-        } else if is_valid(input.trim_right()) {
+        } else if no_anagrams {
+            if is_valid::<AnagramSet>(input.as_ref()) {
+                count += 1;
+            }
+        } else if is_valid::<String>(input.as_ref()) {
             count += 1;
         }
         input.clear();
