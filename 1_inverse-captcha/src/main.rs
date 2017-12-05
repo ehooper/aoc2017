654533aa4dbef6b82084367f301d3a40b29775c2
ehooper <ehooper@umich.edu>
2017-12-05 12:42:17 -0500

    Cleaned up day 1 a little bit.

--- 1_inverse-captcha/src/main.rs
+++ 1_inverse-captcha/src/main.rs
@@ -6,10 +6,10 @@ fn check_consecutive(digits : &[u8]) -> Result<isize, String> {
     let iter = digits.iter().zip(digits.iter().cycle().skip(1));
     for (&d1, &d2) in iter {
         if d1 < b'0' || d1 > b'9' {
-            return Err(format!("error: invalid digit '{}'", d1));
+            return Err(format!("error: invalid digit '{}'", d1 as char));
         }
         if d1 == d2 {
-            sum += (d1 as isize) - (b'0' as isize);
+            sum += (d1 - b'0') as isize;
         }
     }
     Ok(sum)
@@ -17,15 +17,14 @@ fn check_consecutive(digits : &[u8]) -> Result<isize, String> {
 
 fn check_halfway_around(digits : &[u8]) -> Result<isize, String> {
     let mut sum : isize = 0;
-    let nums : Vec<isize> = digits.iter().map(|d| (d - b'0') as isize).collect();
-    let size = nums.len();
-    for (i, &d1) in nums.iter().enumerate() {
-        if d1 < 0 || d1 > 9 {
-            return Err(format!("error: invalid digit '{}'", d1));
+    let size = digits.len();
+    for (i, &d1) in digits.iter().enumerate() {
+        if d1 < b'0' || d1 > b'9' {
+            return Err(format!("error: invalid digit '{}'", d1 as char));
         }
-        let d2 = nums[(i + size / 2) % size];
+        let d2 = digits[(i + size / 2) % size];
         if d1 == d2 {
-            sum += d1;
+            sum += (d1 - b'0') as isize;
         }
     }
     Ok(sum)
@@ -41,10 +40,9 @@ fn main() {
     while let Ok(_) = io::stdin().read_line(&mut input) {
         {
             let bytes = input.trim_right().as_bytes();
-            if part2 {
-                println!("{}", check_halfway_around(bytes).unwrap());
-            } else {
-                println!("{}", check_consecutive(bytes).unwrap());
+            match if part2 { check_halfway_around(bytes) } else { check_consecutive(bytes) } {
+                Ok(sum) => println!("{}", sum),
+                Err(e) => eprintln!("{}", e),
             }
         }
         input.clear();
