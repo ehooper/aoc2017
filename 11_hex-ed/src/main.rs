bb6d3c59574af7329115aa6370e9baa318163b3a
ehooper <ehooper@umich.edu>
2017-12-11 14:04:41 -0500

    Day 11

---
+++ 11_hex-ed/src/main.rs
@@ -0,0 +1,112 @@
+#[derive(Debug, Clone, Copy)]
+enum HexDir {
+    N,
+    NE,
+    SE,
+    S,
+    SW,
+    NW,
+}
+
+use HexDir::*;
+
+impl HexDir {
+    fn apply_step(self, (x, y) : (i32, i32)) -> (i32, i32) {
+        match self {
+            N  => (x    , y + 2),
+            NE => (x + 1, y + 1),
+            SE => (x + 1, y - 1),
+            S  => (x    , y - 2),
+            SW => (x - 1, y - 1),
+            NW => (x - 1, y + 1),
+        }
+    }
+}
+
+use std::str::FromStr;
+
+impl FromStr for HexDir {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_ref() {
+            "n"  => Ok(N),
+            "ne" => Ok(NE),
+            "se" => Ok(SE),
+            "s"  => Ok(S),
+            "sw" => Ok(SW),
+            "nw" => Ok(NW),
+            _ => Err(format!("could not parse direction: '{}'", s)),
+        }
+    }
+}
+
+fn distance((x, y) : (i32, i32)) -> i32 {
+    let xd = x.abs();
+    let yd = y.abs();
+    xd + i32::max(0, (yd - xd) / 2)
+}
+
+struct PathDistance {
+    steps : i32,
+    max : i32,
+}
+
+fn path_distance(path : &[HexDir]) -> PathDistance {
+    let mut max = 0;
+    let d = path.iter().fold((0, 0), |acc, hd| {
+        let next = hd.apply_step(acc);
+        max = i32::max(max, distance(next));
+        next
+    });
+    PathDistance { steps: distance(d), max }
+}
+
+fn parse_input(input : &str) -> Result<Vec<HexDir>, String> {
+    input.split(',').map(str::trim).filter(|s| ! s.is_empty()).map(str::parse).collect()
+}
+
+fn prompt() {
+    use std::io::Write;
+
+    print!("> ");
+    std::io::stdout().flush().unwrap();
+}
+
+fn main() {
+    use std::io::prelude::*;
+
+    let mut input = String::new();
+
+    let run = |input : &str| {
+        let parsed_input = parse_input(input);
+        if let Ok(input) = parsed_input {
+            let answer = path_distance(&input);
+            println!("steps: {}", answer.steps);
+            println!("max: {}", answer.max);
+        } else {
+            eprintln!("{}", parsed_input.unwrap_err());
+        }
+    };
+
+    if let Some(filename) = std::env::args_os().nth(1) {
+        let mut file = std::fs::File::open(filename).expect("file not found");
+        file.read_to_string(&mut input).expect("error reading file");
+        run(&input);
+        return;
+    }
+
+    prompt();
+    while let Ok(_) = std::io::stdin().read_line(&mut input) {
+        run(&input);
+        input.clear();
+        prompt();
+    }
+}
+
+#[test]
+fn test_part_1() {
+    assert_eq!(3, path_distance(&[NE, NE, NE]).steps);
+    assert_eq!(0, path_distance(&[NE, NE, SW, SW]).steps);
+    assert_eq!(2, path_distance(&[NE, NE, S, S]).steps);
+    assert_eq!(3, path_distance(&[SE, SW, SE, SW, SW]).steps);
+}
